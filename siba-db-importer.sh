9d0be6de02526f0d9bd0bb36948381acc1ba43d8
Mauricio Muriel <mauricio.muriel@calitek.net>
2023-04-21 16:24:35 -0500

    Agregando soporte para diferentes fuentes en el archivo de importación

--- siba-db-importer.sh
+++ siba-db-importer.sh
@@ -2,10 +2,15 @@
 #
 #1. Parametros recibidos por la linea de comandos, definidos como opciones
 #
-#    -d -> DB Name
-#    -c -> (Containerized) It indicates whether mysql/mariadb is runing inside a container technology or not.
-#    -f -> How many files clean_db-$3.sql must the script iterates. (optional)
-#    -i -> Input file name, without extension file name (optional)
+#    -d ->  DB Name
+#    -c ->  (Containerized) It indicates whether mysql/mariadb is runing inside a container technology or not.
+#    -f ->  How many files clean_db-$3.sql must the script iterates. (optional)
+#    -i ->  Input file name, without extension file name (optional)
+#    -s ->  Source, The source where the file comes from, if not defined, it takes WEB_SOURCE from .env
+#           It supports:
+#           - http resources (web resource)
+#           - local resource (same directory as PATH_TO_FILES in .env)
+#           - File system resource (path to another local directory)
 
 
 #Importa los datos de configuracion del archivo .env
@@ -19,14 +24,16 @@ dbName=""
 qtyCleanFiles=0
 containerized="0"
 inputFileName=""
+sourceFile=""
 
 # Analizar las opciones de línea de comandos
-while getopts ":d:c:f:i:" opt; do
+while getopts ":d:c:f:i:s:" opt; do
   case $opt in
     d) dbName=$OPTARG;;
     c) containerized=$OPTARG;;
     f) qtyCleanFiles=$OPTARG;;
     i) inputFileName=$OPTARG;;
+    s) sourceFile=$OPTARG;;
     \?) echo "Opción inválida: -$OPTARG" >&2;;
     :) echo "La opción -$OPTARG requiere un argumento." >&2;;
   esac
@@ -52,8 +59,30 @@ fi
 
 rm -f "$PATH_TO_FILES/$inputFileName.sql.tar.gz"
 rm -f "$PATH_TO_FILES/$inputFileName.sql"
-echo "Recuperando el archivo SQL de la fuente $WEB_SOURCE/$inputFileName.sql.tar.gz"
-wget -O "$PATH_TO_FILES/$inputFileName.sql.tar.gz" "$WEB_SOURCE/$inputFileName.sql.tar.gz"
+
+if [[ "$sourceFile" == "local" ]]
+then
+    echo "Tomando el base de la carpeta: $PATH_TO_FILES"
+else
+    if [[ -z sourceFile ]]
+    then
+        #default behavior
+        echo "Recuperando el archivo SQL de la fuente $WEB_SOURCE/$inputFileName.sql.tar.gz"
+        wget -O "$PATH_TO_FILES/$inputFileName.sql.tar.gz" "$WEB_SOURCE/$inputFileName.sql.tar.gz"
+    else
+        httpRegexp = "^http"
+        if [[ $sourceFile =~ $httpRegexp ]]
+        then
+            echo "Recuperando el archivo SQL de la fuente $sourceFile/$inputFileName.sql.tar.gz"
+            wget -O "$PATH_TO_FILES/$inputFileName.sql.tar.gz" "$sourceFile/$inputFileName.sql.tar.gz"    
+        else
+            if [[ "$PATH_TO_FILES" != "$sourceFile" ]]
+            then
+                cp "$sourceFile/$inputFileName.sql.tar.gz" "$PATH_TO_FILES/$inputFileName.sql.tar.gz"
+            fi    
+        fi 
+    fi
+fi
 pwd
 cd $PATH_TO_FILES
 pwd
